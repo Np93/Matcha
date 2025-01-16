12c2256c8ab6c52b43cb370ef2af70fc35cf61e7
Np93 <nico.hir@hotmail.com>
2025-01-16 20:47:11 +0100

    modification frontend, maintenant affiche info sur page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Importez "react-dom/client" pour React 18
 import App from './App';
 
-ReactDOM.render(
+// Récupérer l'élément root dans le DOM
+const rootElement = document.getElementById('root');
+
+// Créez un root React
+const root = ReactDOM.createRoot(rootElement);
+
+// Rendre l'application dans le root React
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
